d78992f03be6d794aebb50c2d7e2f70927a1b0b6
Eric Sampaio <erick.gtn.xp@gmail.com>
2022-04-02 21:42:08 -0300

    Atividade 1 em python

---
+++ Atividade_1.py
@@ -0,0 +1,9 @@
+import math
+import sys
+
+num1_str = input ("Informe um valor: ")
+num2_str = input ("Informe um outro valor: ")
+num1=int(num1_str)
+num2=int(num2_str)
+soma = num1+num2
+print("O resultado da soma é:", soma)
